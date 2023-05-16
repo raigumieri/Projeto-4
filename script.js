8be6e82a3898023eca3f8327300e2e25d6a75b7b
raigumieri <rai.gumiere@hotmail.com>
2023-05-16 14:22:56 -0300

    Projeto 4 JavaScript

---
+++ script.js
@@ -0,0 +1,8 @@
+function CalQuadrado(){
+    let numeros = document.getElementById("numeros").value;
+    numeros = numeros.split(",");
+    for (i = 0; i < numeros.length; i++){
+        quadrado = parseInt(numeros[i]) * parseInt(numeros[i]);
+        document.write("O quadrado de " +numeros[i]+ " é " +quadrado+ "<br>");
+    }
+}
